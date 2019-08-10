9fee2851584102f987338c6b872646c321947bd1
Akhil Narang <akhilnarang.1999@gmail.com>
2019-08-10 10:06:12 +0200

    DumpyAra: Let Aria2C Enjoy
    Signed-off-by: Atman <masteratman@gmail.com>

--- dumpyara.sh
+++ dumpyara.sh
@@ -18,7 +18,7 @@ fi
 URL=$1
 if echo "$1" | grep "http" ; then
 	cd $PROJECT_DIR/input
-	aria2c -x16 ${URL:?} || wget ${URL:?} || exit 1 #download rom
+	aria2c -x16 -j$(nproc) ${URL} || wget ${URL} || exit 1
 else
 	cp -a "$1" $PROJECT_DIR/input
 fi
