785cef40ad2c380db63a54412b6366b6eaef3584
ShubhamB <shubhamprince111@gmail.com>
2020-03-25 17:28:19 +0530

    Install docopt with pip
     * Needed for oppo_ozip_decrypt

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2
-    pip3 install backports.lzma protobuf pycrypto
+    pip3 install backports.lzma protobuf pycrypto docopt
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install protobuf xz brotli lz4 aria2
-    pip install backports.lzma protobuf pycrypto
+    pip install backports.lzma protobuf pycrypto docopt
 fi
