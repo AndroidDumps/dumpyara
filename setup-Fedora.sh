d6fb0fc77cea27858ad165f183155d859f5c6aaa
loserq <2650009628@qq.com>
2020-07-15 21:15:47 +0800

    setup-Fedora.sh: Add setup for Fedora31

---
+++ setup-Fedora.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+    #Fedora31 Monopolize
+    sudo dnf install python3-devel xz-devel p7zip msgpack lzma-sdk-devel lz4-devel 
+    #Linux-gun
+    sudo dnf install unace unrar zip unzip sharutils uudeview arj cabextract file-roller dtc python3-pip brotli axel gawk aria2 detox
+    sudo pip3 install backports.lzma protobuf pycrypto docopt
+
