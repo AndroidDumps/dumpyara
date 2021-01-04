759cf6cf0e4659bd79578b1516bdd269f98c75c3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-04 21:50:31 +0100

    setup.sh: Add support to Arch Linux

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename
+    if [[ "$(command -v apt)" != "" ]]; then
+        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename
+    elif [[ "$(command -v pacman)" != "" ]]; then
+        sudo pacman -Sy --noconfirm unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python-pip brotli axel gawk aria2 detox cpio
+    fi
     PIP=pip3
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install protobuf xz brotli lz4 aria2
