e54ee0a49902768f3a8c0693656e6b7723a79517
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-04 21:22:31 +0100

    setup.sh: Commonize pip packages

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename
-    pip3 install backports.lzma protobuf pycrypto docopt zstandard
+    PIP=pip3
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install protobuf xz brotli lz4 aria2
-    pip install backports.lzma protobuf pycrypto docopt zstandard
+    PIP=pip
 fi
+
+"$PIP" install backports.lzma protobuf pycrypto docopt zstandard
