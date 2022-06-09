421ac755a6996ceb5ad06f63c5f6d860fddb8aa4
Pronoy Mukherjee <83197609+pronoy2108@users.noreply.github.com>
2022-06-09 16:19:43 +0530

    Create setup-win.ps1

---
+++ setup-win.ps1
@@ -0,0 +1,5 @@
+# Install scoop
+irm get.scoop.sh | iex
+
+# Other tools for Windows/ReactOS
+scoop install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev
