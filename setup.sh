46146fcb5cef2302da9c0bb7a624599044dd6d35
OneNormalUsername <42769194+OneNormalUsername@users.noreply.github.com>
2022-09-19 20:23:19 +0200

    Install curl and sync $PARTITIONS with dumpbot
    curl is not present out of the box in Ubuntu 22.04.1 LTS

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     if [[ "$(command -v apt)" != "" ]]; then
-        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev -y
+        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev curl -y
     elif [[ "$(command -v dnf)" != "" ]]; then
         sudo dnf install -y unace unrar zip unzip sharutils uudeview arj cabextract file-roller dtc python3-pip brotli axel aria2 detox cpio lz4 python3-devel xz-devel p7zip p7zip-plugins
     elif [[ "$(command -v pacman)" != "" ]]; then
