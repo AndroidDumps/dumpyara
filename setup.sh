cde0a28367831c44e6813a8c18a4b6404bc679f6
Ahmed Harhash <email@lacentix.me>
2023-03-03 14:40:44 +0200

    setup.sh: Determine which command to use for privilege escalation

--- setup.sh
+++ setup.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# Determine which command to use for privilege escalation
+if command -v sudo > /dev/null 2>&1; then
+    sudo_cmd="sudo"
+elif command -v doas > /dev/null 2>&1; then
+    sudo_cmd="doas"
+else
+    echo "Neither sudo nor doas found. Please install one of them."
+    exit 1
+fi
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     if [[ "$(command -v apt)" != "" ]]; then
-        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev curl -y
+        $sudo_cmd apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev curl -y
     elif [[ "$(command -v dnf)" != "" ]]; then
-        sudo dnf install -y unace unrar zip unzip sharutils uudeview arj cabextract file-roller dtc python3-pip brotli axel aria2 detox cpio lz4 python3-devel xz-devel p7zip p7zip-plugins
+        $sudo_cmd dnf install -y unace unrar zip unzip sharutils uudeview arj cabextract file-roller dtc python3-pip brotli axel aria2 detox cpio lz4 python3-devel xz-devel p7zip p7zip-plugins
     elif [[ "$(command -v pacman)" != "" ]]; then
-        sudo pacman -Sy --noconfirm --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python-pip brotli axel gawk aria2 detox cpio lz4
+        $sudo_cmd pacman -Sy --noconfirm --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python-pip brotli axel gawk aria2 detox cpio lz4
     fi
     PIP=pip3
 elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -14,4 +24,4 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     PIP=pip
 fi
 
-sudo "$PIP" install backports.lzma extract-dtb protobuf pycrypto docopt zstandard twrpdtgen
+$sudo_cmd "$PIP" install backports.lzma extract-dtb protobuf pycrypto docopt zstandard twrpdtgen
