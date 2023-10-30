6d9d983a6f6e849e083b006b2651f7e35234e14a
SH4ND <miqueiasantos20@gmail.com>
2023-10-30 14:57:40 -0300

    setup.sh: Introduce opensuse support

--- setup.sh
+++ setup.sh
@@ -15,6 +15,8 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
         $sudo_cmd apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev curl python3-venv -y
     elif [[ "$(command -v dnf)" != "" ]]; then
         $sudo_cmd dnf install -y unace unrar zip unzip sharutils uudeview arj cabextract file-roller dtc python3-pip brotli axel aria2 detox cpio lz4 python3-devel xz-devel p7zip p7zip-plugins
+    elif [[ "$(command -v zypper)" != "" ]]; then
+        $sudo_cmd zypper install -y unrar zip unzip sharutils uudeview arj cabextract file-roller dtc python3-pip brotli axel aria2 renameutils cpio lz4 python3-devel xz-devel p7zip
     elif [[ "$(command -v pacman)" != "" ]]; then
         $sudo_cmd pacman -Sy --noconfirm --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python-pip brotli axel gawk aria2 detox cpio lz4
     fi
