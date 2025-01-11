d418a18f035fc206d8cfe1931326fd1be856635c
ant <ant@national.shitposting.agency>
2025-01-11 14:53:15 +0100

    fix!: remove 'brew' from 'setup.sh'
    Change-Id: I6b06e7099e208dd9fd70a2c6bb60baa83e4ff13b

--- setup.sh
+++ setup.sh
@@ -10,19 +10,14 @@ else
     exit 1
 fi
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    if [[ "$(command -v apt)" != "" ]]; then
-        $sudo_cmd apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev curl python3-venv -y
-    elif [[ "$(command -v dnf)" != "" ]]; then
-        $sudo_cmd dnf install -y unace unrar zip unzip sharutils uudeview arj cabextract file-roller dtc python3-pip brotli axel aria2 detox cpio lz4 python3-devel xz-devel p7zip p7zip-plugins
-    elif [[ "$(command -v pacman)" != "" ]]; then
-        $sudo_cmd pacman -Sy --noconfirm --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python-pip brotli axel gawk aria2 detox cpio lz4
-    fi
-    PIP=pip3
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install protobuf xz brotli lz4 aria2 detox coreutils p7zip gawk
-    PIP=pip
+if [[ "$(command -v apt)" != "" ]]; then
+    $sudo_cmd apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev curl python3-venv -y
+elif [[ "$(command -v dnf)" != "" ]]; then
+    $sudo_cmd dnf install -y unace unrar zip unzip sharutils uudeview arj cabextract file-roller dtc python3-pip brotli axel aria2 detox cpio lz4 python3-devel xz-devel p7zip p7zip-plugins
+elif [[ "$(command -v pacman)" != "" ]]; then
+    $sudo_cmd pacman -Sy --noconfirm --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python-pip brotli axel gawk aria2 detox cpio lz4
 fi
+PIP=pip3
 
 # Create virtual environment and install packages
 python3 -m venv .venv
