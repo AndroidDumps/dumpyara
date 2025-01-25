f45ff916cb5032730603d74e6f4f48bdf84a812e
Secondarian <101978322+Secondarian@users.noreply.github.com>
2025-01-25 17:21:16 +0100

    setup.sh: Add su fallback (#63)
    Makes setup.sh work without sudo or doas installed
    
    Change-Id: I6e7335659b8482f4bebd5bf266e8ef2cbe8d1d61

--- dumpyara.sh
+++ dumpyara.sh
@@ -17,8 +17,12 @@ if command -v sudo > /dev/null 2>&1; then
 elif command -v doas > /dev/null 2>&1; then
     sudo_cmd="doas"
 else
-    echo "Neither sudo nor doas found. Please install one of them."
-    exit 1
+    echo "[WARN] Neither 'sudo' nor 'doas' found; resorting to 'su'."
+    # Create a separated function in order to handle 'su'
+    su_cmd() { 
+        su -c "$*" 
+    }
+    SUPERUSER="su_cmd"
 fi
 
 # Activate virtual environment
